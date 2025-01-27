ed4acf1c1520d83fc9064e7b66338c89d44773e3
Alisonrajpal <alisonrajpal@gmail.com>
2025-01-27 22:33:55 +0200

    Create a component with weather component

---
+++ src/components/Weather.js
@@ -0,0 +1,11 @@
+import React from "react";
+export default function Weather() {
+  return (
+    <div>
+      <form>
+        <input type="search" placeholder="Enter your city here ..." />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
+}
