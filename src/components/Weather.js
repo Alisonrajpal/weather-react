56b854ad5cc04c5b99c54126bf57efad0ced7893
Alisonrajpal <alisonrajpal@gmail.com>
2025-01-27 23:09:06 +0200

    added a footer

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -6,6 +6,17 @@ export default function Weather() {
         <input type="search" placeholder="Enter your city here ..." />
         <button type="submit">Search++</button>
       </form>
+      <div>
+        <p>
+          Code On{" "}
+          <a
+            href="https://github.com/Alisonrajpal/weather-react"
+            target="_/blank">
+            Github
+          </a>
+          and coded by Alison Rajpal
+        </p>
+      </div>
     </div>
   );
 }
